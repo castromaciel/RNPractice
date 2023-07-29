e430d192d377eaef4753aa965b50a37b5259f5d8
castromaciel <castromaciel00@gmail.com>
2023-07-29 14:16:08 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+export default App;
