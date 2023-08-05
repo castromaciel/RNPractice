ef2b5960649c6d27f034b64a85083b08c7eadda1
castromaciel <castromaciel00@gmail.com>
2023-08-05 14:14:41 -0300

    position absolute

--- src/screens/PositionScreen/PositionScreen.tsx
+++ src/screens/PositionScreen/PositionScreen.tsx
@@ -3,8 +3,9 @@ import { StyleSheet, View } from 'react-native';
 const PositionScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={[styles.box_lg, styles.purple]}></View>
-      <View style={[styles.box_lg, styles.orange]}></View>
+      <View style={[styles.box_lg, styles.first]}></View>
+      <View style={[styles.box_lg, styles.second]}></View>
+      <View style={[styles.box_lg, styles.third]}></View>
     </View>
   )
 }
@@ -14,19 +15,28 @@ const styles = StyleSheet.create({
     height: 100,
     width: 100,
     borderWidth: 2,
-    borderColor: '#FFFFFF',
+    borderColor: '#1E2632',
   },
   container: {
     backgroundColor: '#0D1117',
     flex: 1,
   },
-  orange: {
-    backgroundColor: '#F0A23B'
+  first: {
+    backgroundColor: '#465162',
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
   },
-  purple: {
-    backgroundColor: '#800080',
-    top: 100,
-    left: 100
+  second: {
+    backgroundColor: '#173767',
+    position: 'absolute',
+    top: 0,
+    right: 0
+  },
+  third: {
+    backgroundColor: '#255AAB',
+    position: 'absolute',
+    botom: 0,
   },
 });
 
